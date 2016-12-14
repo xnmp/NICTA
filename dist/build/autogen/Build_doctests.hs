d3dd1b7fa86095c193d7f4ace6e2d584140ab4ba
xnmp <pooky472@hotmail.com>
2016-12-15 01:31:03 +1100

    Done with the lists

---
+++ dist/build/autogen/Build_doctests.hs
@@ -0,0 +1,3 @@
+module Build_doctests where
+deps :: [String]
+deps = ["QuickCheck-2.9.2","array-0.5.1.0","base-4.8.2.0","containers-0.5.6.2","doctest-0.11.0","directory-1.2.2.0","filepath-1.4.0.0","template-haskell-2.10.0.0"]
---
+++ dist/build/autogen/Paths_course.hs
@@ -0,0 +1,35 @@
+module Paths_course (
+    version,
+    getBinDir, getLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,4] []
+bindir, libdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/home/chong/.cabal/bin"
+libdir     = "/home/chong/.cabal/lib/x86_64-linux-ghc-7.10.3/course-0.1.4-K3VxOpHiVk9K0JM0rO1dm2"
+datadir    = "/home/chong/.cabal/share/x86_64-linux-ghc-7.10.3/course-0.1.4"
+libexecdir = "/home/chong/.cabal/libexec"
+sysconfdir = "/home/chong/.cabal/etc"
+
+getBinDir, getLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "course_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "course_libdir") (\_ -> return libdir)
+getDataDir = catchIO (getEnv "course_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "course_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "course_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
